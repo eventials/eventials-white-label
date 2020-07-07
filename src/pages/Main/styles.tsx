6f6fa6b2261518a7e5712ba0ca34283a317c03c3
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-07 18:13:57 -0300

    EVTS-802 - Created and import custom carousel

---
+++ src/pages/Main/styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Toolbar = styled.div`
+  background-color: #152e4d;
+  width: 100%;
+  z-index: 2;
+  display: -ms-flexbox;
+  display: flex;
+  height: 70px;
+  -ms-flex-pack: center;
+  justify-content: center;
+`;
