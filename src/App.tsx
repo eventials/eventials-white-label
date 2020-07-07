7bf8e97894c24ca48a20c49ae7473ff574a2c8b0
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-07 14:22:34 -0300

    EVTS-802 - Create Main to import Whitelabels

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import Main from './pages/Main';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
