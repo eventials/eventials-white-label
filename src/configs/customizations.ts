9c6b791cf604b9a2ba71b15119efa818824075c9
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-15 14:11:46 -0300

    EVTS-805 -  Added file of configs

---
+++ src/configs/customizations.ts
@@ -0,0 +1,21 @@
+export const configs = {
+  logo: 'https://s3.amazonaws.com/dev-assets.eventials.com/logo.png',
+  descriptionImage:
+    'https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/banner_texto_vazio_cortado.png',
+  banner: [
+    'https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/Banner2.jpg',
+    'https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/Banner1.jpg',
+  ],
+  bannerContact:
+    'https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/background_formulario.png',
+  name: 'SND DISTRIBUIÇÃO DE PRODUTOS DE INFORMÁTICA SA',
+  adress: 'AV. COPACABANA, 238 – BARUERI – SP - 06472-001',
+  phone: 'Tel.: (11) 2187-8400',
+  site: 'https://www.snd.com.br/',
+  imageTitleContact:
+    'https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/title_formulario.png',
+  youtube: 'https://www.youtube.com/channel/UCWXVR2HZGRoyTCmXlh98VSQ',
+  linkedIn: 'https://www.linkedin.com/company/snd/',
+  facebook: 'https://www.facebook.com/SNDDistribuidora/',
+  instagram: 'https://www.instagram.com/sndmais/',
+};
