bf63d3ea29947a524870b89dbd916223b92dda6d
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-15 14:15:43 -0300

    EVTS-805 - Added dynimc in banner carousel

--- src/components/BannerCarousel/index.tsx
+++ src/components/BannerCarousel/index.tsx
@@ -1,32 +1,15 @@
+import { configs } from 'configs/customizations';
 import * as React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 function BannerCarousel() {
   return (
     <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/Banner2.jpg"
-          alt="First slide"
-        />
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://s3.amazonaws.com/static.eventials.com/whitelabel-snd/Banner1.jpg"
-          alt="Third slide"
-        />
-
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
+      {configs?.banner.map(b => (
+        <Carousel.Item>
+          <img className="d-block w-100" src={b} alt="Banner" />
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
