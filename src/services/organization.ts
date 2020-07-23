0fa7cfa6d21917e5c14377daf84c3827e0d759a2
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-23 17:13:56 -0300

    EVTS-805 - Added methods to refreshToken

--- src/services/organization.ts
+++ src/services/organization.ts
@@ -5,3 +5,11 @@ export const setToken = (token: string) => {
 export const getToken = (): any => {
   return localStorage?.getItem('token');
 };
+
+export const setRefreshToken = (token: string) => {
+  return localStorage.setItem('refresh_token', token);
+};
+
+export const getRefreshToken = (): any => {
+  return localStorage?.getItem('refresh_token');
+};
